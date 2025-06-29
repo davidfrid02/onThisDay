0bf853196ac297eff3f89952e6fd14dcb2af213e
David Fried <david.fried@firefly.ai>
2025-06-29 10:06:55 +0300

    adjust the length of the tweet

--- src/services/insightaServices.js
+++ src/services/insightaServices.js
@@ -14,8 +14,8 @@ class InsightaServices {
             const randomEvent = events[randomIndex];
             let fact = `${randomEvent.year}: ${randomEvent.text}`;
 
-            if (fact.length > 250) {
-                fact = fact.slice(0, 247) + "...";
+            if (fact.length > 280) {
+                fact = fact.slice(0, 277) + "...";
             }
             console.log(`Today fact: ${fact}\n`);
             return fact;
